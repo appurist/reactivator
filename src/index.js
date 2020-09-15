a9d54b4ffa160bca0d0d4e15c6c1949dc108d891
Appurist / Paul <dev@appurist.com>
2020-09-15 00:31:27 -0300

    Added reactive() and some test/example uses of it.

--- src/index.js
+++ src/index.js
@@ -9,11 +9,68 @@ class Ref {
   }
 
   set value (val) { 
+    let old = this._value;
     this._value = val;
     // notify observers
     this._subs.forEach(subFunc => 
-      subFunc()
+      subFunc(old, val)      
     );
+  }
+
+  watch (func) {
+    if (!this._subs.includes(func)) {
+      this._subs.push(func);
+    }
+  }  
+
+  unwatch (func) {
+    if (this._subs.includes(func)) {
+      this._subs = this._subs.filter(f => f !== func);
+    }
+  }  
+}
+
+class Reactive {
+  constructor(obj) {
+    let _this = this;
+    this._obj = obj;
+    this._subs = [ ];
+
+    this.handler = {
+      get (target, prop, receiver) {
+        if (prop === 'watch') return _this.watch;
+        if (prop === 'unwatch') return _this.unwatch;
+        if (prop === '_subs') return _this._subs;
+
+        // console.log(`Proxy get for '${prop}'`);
+        return obj[prop];
+      },
+      set (obj, prop, value) {
+        if (prop === 'watch') return;
+        if (prop === 'unwatch') return;
+        if (prop === '_subs') return _this._subs = value;
+
+        // console.log(`Proxy set for '${prop}' from ${old} to ${value}`); 
+        let old = obj[prop];
+        obj[prop] = value;
+        // notify observers
+        _this._subs.forEach(subFunc => 
+          subFunc(obj, prop, old, value)
+        );
+        return obj[prop];
+      }
+    }
+
+    this.obj = new Proxy(obj, this.handler);
+    // this.obj.watch = this.watch;
+    return this.obj;
+  }
+  
+  get value () {
+    return this.obj;
+  }
+  set value (val) {
+    console.error("You cannot set the value of a reactive() object itself, set data members only.") 
   }
 
   watch (func) {
@@ -40,4 +97,8 @@ function ref(thing) {
   return new Ref(thing);
 }
 
-export { ref, watch, unwatch };
+function reactive(thing) {
+  return new Reactive(thing);
+}
+
+export { ref, reactive, watch, unwatch };
--- src/test.mjs
+++ src/test.mjs
@@ -1,25 +1,30 @@
-import { ref, watch, unwatch } from '../dist/reactivator.esm.js'
+//import { ref, reactive, watch, unwatch } from '../dist/reactivator.esm.js'
+import { ref, reactive, watch, unwatch } from '../src/index.js'
 
 let state = {
   test1: ref('Hi there'),
   test2: ref(5),
+  test3: reactive({field1: 42, field2: 'hello'}),
   init() {
     console.log('test1 =', this.test1.value)
     console.log('test2 =', this.test2.value)
+    console.log('test3 =', this.test3)
 
-    this.test1.watch(() =>
-      console.log('test1 changed to', this.test1.value)
-    )
-    this.test2.watch(() => 
-      console.log('test2 changed to', this.test2.value)
-    )
+    this.test1.watch((old, val) => {
+      console.log(`test1.value changed from ${old} to ${val}`);
+    })
+    this.test2.watch((old, val) => {
+    console.log(`test2.value changed from ${old} to ${val}`);
+    })
+    this.test3.watch((obj, prop, old, val) => {
+      console.log(`test3.${prop} changed from ${old} to ${val}`)
+    })
   }
 }
 
 state.init();
 
 state.test1.value = 'Hello';
-console.log('test1 =', state.test1.value);
 
 // Now add another "top-level" watch AFTER the first change.
 let saved = undefined;
@@ -28,18 +33,29 @@ watch(state.test1, saved = () => {
 })
 
 state.test1.value += ' world';
-console.log('test1 =', state.test1.value);
 
 // We should only see the one "Top-level" change notification above for "Hello there"
 unwatch(state.test1, saved);
 
 state.test1.value = 'Goodbye!';
-console.log('test1 =', state.test1.value);
 
 //////////////////////////////////////////////////////////////////////////
 // Repeat a couple of changes with numbers
 state.test2.value = 6;
-console.log('test2 =', state.test2.value);
 
 state.test2.value++;
-console.log('test2 =', state.test2.value);
+
+//////////////////////////////////////////////////////////////////////////
+// Now repeat with an object
+state.test3.field1 = 99;
+let saved3 = undefined;
+watch(state.test3, saved3 = (obj, prop, old, val) => { 
+  console.log(`Top-level watch noticed test3.${prop} changed from ${old} to ${val}`);
+})
+
+state.test3.field1 = 100;
+state.test3.field3 = 'completely new';
+
+unwatch(state.test3, saved3);
+
+state.test3.field2 = 'Goodbye';
