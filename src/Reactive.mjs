76c616931c858558ceb61f655babed1d488501fe
Appurist / Paul <dev@appurist.com>
2020-09-15 14:56:33 -0300

    Added support for tracking nested object names with a '.subfield' label.

--- src/Reactive.mjs
+++ src/Reactive.mjs
@@ -1,17 +1,19 @@
-var reactiveHandler = function (instance) {
+var reactiveHandler = function (instance, label) {
 	return {
     get (obj, prop, receiver) {
       if (prop === '_reactive') return instance._reactive;
       if (prop === '_subs') return instance._subs;
       if (prop === '_label') return instance._label;
+      if (prop === '_options') return instance._options;
 
       // console.log(`Proxy get for '${prop}'`);
       // See https://gomakethings.com/how-to-create-a-reactive-state-based-ui-component-with-vanilla-js-proxies/
       // and https://gomakethings.com/how-to-detect-changes-to-nested-arrays-and-objects-inside-a-proxy/
       // for this bit of very clever voodoo for the conditional.
       if (['[object Object]', '[object Array]'].indexOf(Object.prototype.toString.call(obj[prop])) > -1) {
-        console.log(`>> New proxy for ${prop}`);
-        return new Proxy(obj[prop], reactiveHandler(instance));
+        let newLabel = `${label || ''}.${prop}`;
+        //console.log(`>> New proxy for ${newLabel}`);
+        return new Proxy(obj[prop], reactiveHandler(instance, newLabel));
       }        
 
       return obj[prop];
@@ -21,18 +23,19 @@ var reactiveHandler = function (instance) {
       // if (prop === '_reactive') return instance._reactive = value;
       if (prop === '_subs') return instance._subs = value;
       if (prop === '_label') return instance._label = value;
+      if (prop === '_options') return instance._options = value;
 
       // console.log(`Proxy set for '${prop}' from ${old} to ${value}`); 
       let old = obj[prop];
       obj[prop] = value;
       // notify observers
       instance._subs.forEach(subFunc => 
-        subFunc(obj, prop, old, value)
+        subFunc(label, prop, old, value, obj)
       );
       return obj[prop];
     },
     deleteProperty: function (obj, prop) {
-      console.log(`<< Deleting prop: ${prop}`);
+      // console.log(`<< Deleting prop: ${prop}`);
       delete obj[prop];
       return true;
     }      
@@ -40,13 +43,14 @@ var reactiveHandler = function (instance) {
 }
 
 class Reactive {
-  constructor(obj, label) {
+  constructor(obj, label, options) {
     this._obj = obj;
     this._subs = [ ];
-    this.obj = new Proxy(obj, reactiveHandler(this));
+    this.obj = new Proxy(obj, reactiveHandler(this, label));
     // this.obj._reactive = this;
     this._reactive = this;
     this._label = label;
+    this._options = options;
     return this.obj;
   }
 }
